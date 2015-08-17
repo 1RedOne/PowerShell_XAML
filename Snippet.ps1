fead2b218930f53626a515b5776d71f4b62b471f
Stephen Owen <sred13@gmail.com>
2015-08-17 11:27:19 -0400

    First post
    Not going to be updating this on the blog anymore, instead, updates will
    live here on github from now on

---
+++ Snippet.ps1
@@ -0,0 +1,70 @@
+﻿    #ERASE ALL THIS AND PUT XAML BELOW between the @" "@ 
+$inputXML = @"
+<Window x:Class="WpfApplication2.MainWindow"
+ xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+ xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+ xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+ xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+ xmlns:local="clr-namespace:WpfApplication2"
+ mc:Ignorable="d"
+ Title="FoxDeploy Awesome Tool" Height="416.794" Width="598.474" Topmost="True">
+ <Grid Margin="0,0,45,0">
+ </Grid>
+</Window>
+
+"@ 
+
+$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
+[void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[xml]$XAML = $inputXML
+#Read XAML
+
+    $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
+  try{$Form=[Windows.Markup.XamlReader]::Load( $reader )}
+catch [System.Management.Automation.MethodInvocationException] {
+    Write-Warning "We ran into a problem with the XAML code.  Check the syntax for this control..."
+    write-host $error[0].Exception.Message -ForegroundColor Red
+    if ($error[0].Exception.Message -like "*button*"){
+        write-warning "Ensure your &lt;button in the `$inputXML does NOT have a Click=ButtonClick property.  PS can't handle this`n`n`n`n"}
+}
+catch{#if it broke some other way :D
+    Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."
+        }
+
+#===========================================================================
+# Store Form Objects In PowerShell
+#===========================================================================
+
+$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+
+Function Get-FormVariables{
+if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true}
+write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+get-variable WPF*
+}
+
+Get-FormVariables
+
+#===========================================================================
+# Use this space to add code to the various form elements in your GUI
+#===========================================================================
+                                                                   
+    
+#Reference 
+
+#Adding items to a dropdown/combo box
+    #$vmpicklistView.items.Add([pscustomobject]@{'VMName'=($_).Name;Status=$_.Status;Other="Yes"})
+    
+#Setting the text of a text box to the current PC name    
+    #$WPFtextBox.Text = $env:COMPUTERNAME
+    
+#Adding code to a button, so that when clicked, it pings a system
+# $WPFbutton.Add_Click({ Test-connection -count 1 -ComputerName $WPFtextBox.Text
+# })
+#===========================================================================
+# Shows the form
+#===========================================================================
+write-host "To show the form, run the following" -ForegroundColor Cyan
+'$Form.ShowDialog() | out-null'
+
+
